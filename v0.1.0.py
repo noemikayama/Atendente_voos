d6de059b1ca18f6ca6315a0fdb82b5e744007639
noemikayama <n_kayama@hotmail.com>
2025-05-25 15:29:45 -0300

    voos

--- v0.1.0.py
+++ v0.1.0.py
@@ -1,12 +1,12 @@
-# -----*----- ATENDENTE DE VOÔS -----*-----
+# -----*----- ATENDENTE DE VOOS -----*-----
 
 def exibir_menu():
-    print("\n\n\t\t -*- PROGRAMA DE ATENDENTE DE VOÔS -*-")
+    print("\n\n\t\t -*- PROGRAMA DE ATENDENTE DE VOOS -*-")
     print("\n\n\t\t\t >> MENU <<")
-    print("\n 1 - Cadastrar voô")
-    print("\n 2 - Consultar voô")
-    print("\n 3 - Informar voô com menor escala")
-    print("\n 4 - Listar passageiros do voô")
+    print("\n 1 - Cadastrar voo")
+    print("\n 2 - Consultar voo")
+    print("\n 3 - Informar voo com menor escala")
+    print("\n 4 - Listar passageiros do voo")
     print("\n 5 - Venda de passagem")
     print("\n 6 - Cancelamento de passagem")
     print("\n 7 - Sair do programa")
@@ -17,11 +17,11 @@ def exibir_menu():
 
 
 def cadastro_voo():
-    print("\n\n\t\t -*- CADASTRAR UM VOÔ -*- ")
+    print("\n\n\t\t -*- CADASTRAR UM VOOS -*- ")
 
     lista_voos = []
 
-    num1 = int(input("\n\n Insira quantos voôs deseja cadastrar: "))
+    num1 = int(input("\n\n Insira quantos voos deseja cadastrar: "))
 
     for i in range(num1):
         num_voo1 = verificar_voo()
@@ -48,19 +48,19 @@ def cadastro_voo():
 
 
 def mostrar_voos_disponiveis():
-    print("\n\n\t\t -*- VOÔS DISPONÍVEIS -*-")
+    print("\n\n\t\t -*- voos DISPONÍVEIS -*-")
     if not voos:
-        print("\n => NENHUM VOÔ CADASTRADO NO SISTEMA <= ")
+        print("\n => NENHUM VOOS CADASTRADO NO SISTEMA <= ")
     else:
         for i in range (len(lista_voos)) :
             if lista_voos[i][5] > 0 :
-                print(f"\n >> VOÔ {lista_voos[i][0]} | Origem: {lista_voos[i][1]} | Destino: {lista_voos[i][2]} | Escalas: {lista_voos[i][3]} | Preço: R$ {lista_voos[i][4]:.2f} | Lugares disponíveis: {lista_voos[i][5]}")
+                print(f"\n >> Voo: {lista_voos[i][0]} | Origem: {lista_voos[i][1]} | Destino: {lista_voos[i][2]} | Escalas: {lista_voos[i][3]} | Preço: R$ {lista_voos[i][4]:.2f} | Lugares disponíveis: {lista_voos[i][5]}")
 
 
 def menu_consulta():
-    print("\n\n\t\t -*- CONSULTAR VOÔ -*- ")
-    print("\n\n\t Como deseja consultar o voô? ")
-    print("\n 1 - Pelo número do voô")
+    print("\n\n\t\t -*- CONSULTAR VOOS -*- ")
+    print("\n\n\t Como deseja consultar o voo? ")
+    print("\n 1 - Pelo número do voo")
     print("\n 2 - Cidade de origem")
     print("\n 3 - Cidade de destino")
 
@@ -69,14 +69,14 @@ def menu_consulta():
 
 
 def pesquisa_voo():
-    consulta_num = int(input("\n\n Qual voô deseja consultar?"))
+    consulta_num = int(input("\n\n Qual voo deseja consultar?"))
     while (consulta_num not in voos.keys()):
-        print("\n => VOÔ NÃO CADASTRADO, TENTE NOVAMENTE <=")
-        consulta_num = int(input("\n\n Qual voô deseja consultar?"))
+        print("\n => VOOS NÃO CADASTRADO, TENTE NOVAMENTE <=")
+        consulta_num = int(input("\n\n Qual voo deseja consultar?"))
 
     for num_voo, dados in voos.items():
         if consulta_num == num_voo:
-            print(f"\n\n\t\t\t >> VOÔ {num_voo} <<")
+            print(f"\n\n\t\t\t >> VOO {num_voo} <<")
             print(f"\n Cidade de origem: {dados[0]}")
             print(f"\n Cidade de destino: {dados[1]}")
             print(f"\n Número de escalas: {dados[2]}")
@@ -90,12 +90,12 @@ def cidade_origem():
     encontrado = False
     for num_voo, dados in voos.items():
         if (consulta_origem == dados[0].lower()):
-            print(f"\n\n\t\t\t >> VOÔ {num_voo} <<")
+            print(f"\n\n\t\t\t >> voo {num_voo} <<")
             print(f"\n Cidade de destino: {dados[1]}")
             print(f"\n Preço: R$ {dados[3]:.2f}")
             encontrado = True
     if not encontrado:
-        print("\n => NENHUM VOÔ ENCONTRADO PARA ESTA CIDADE DE ORIGEM <=")
+        print("\n => NENHUM VOO ENCONTRADO PARA ESTA CIDADE DE ORIGEM <=")
 
 
 def cidade_destino():
@@ -104,16 +104,16 @@ def cidade_destino():
     encontrado = False
     for num_voo, dados in voos.items():
         if (consulta_destino == dados[1].lower()):
-            print(f"\n\n\t\t\t >> VOÔ {num_voo} <<")
+            print(f"\n\n\t\t\t >> voo {num_voo} <<")
             print(f"\n Cidade de origem: {dados[0]}")
             print(f"\n Preço: R$ {dados[3]:.2f}")
             encontrado = True
     if not encontrado:
-        print("\n => NENHUM VOÔ ENCONTRADO PARA ESTA CIDADE DE DESTINO <=")
+        print("\n => NENHUM VOO ENCONTRADO PARA ESTA CIDADE DE DESTINO <=")
 
 
 def menor_escala():
-    print("\n\n\t\t -*- INFORMAR VOÔ COM MENOR ESCALA -*- ")
+    print("\n\n\t\t -*- INFORMAR VOOS COM MENOR ESCALA -*- ")
 
     escala_origem = input("\n\n Insira a cidade de origem: ").lower()
     escala_destino = input("\n Insira a cidade de destino: ").lower()
@@ -128,16 +128,16 @@ def menor_escala():
                 voo = num_voo
             else:
                 if dados[2] == menor:
-                    print(f"\n\n O voô de {escala_origem.title()} para {escala_destino.title()} com menor escala é o voô {num_voo} com {menor} escalas")
+                    print(f"\n\n O voo de {escala_origem.title()} para {escala_destino.title()} com menor escala é o voo {num_voo} com {menor} escalas")
     
     if voo is not None:
-        print(f"\n\n O voô de {escala_origem.title()} para {escala_destino.title()} com menor escala é o voô {voo} com {menor} escalas")
+        print(f"\n\n O voo de {escala_origem.title()} para {escala_destino.title()} com menor escala é o voo {voo} com {menor} escalas")
     else:
-        print(f"\n => NENHUM VOÔ ENCONTRADO DE {escala_origem.title()} PARA {escala_destino.title()} <= ")
+        print(f"\n => NENHUM VOO ENCONTRADO DE {escala_origem.title()} PARA {escala_destino.title()} <= ")
     
 
 def listar_passageiros():
-    print("\n\n\t\t -*- LISTAR PASSAGEIROS E LUGARES DE UM VOÔ -*-")
+    print("\n\n\t\t -*- LISTAR PASSAGEIROS E LUGARES DE UM VOO -*-")
 
     consulta_voo = verificar_voo_compra()
     
@@ -147,8 +147,8 @@ def listar_passageiros():
                 for r in range(len(dados[5])):
                     print(f"\n\t Passageiro(a) {r+1}: {dados[5][r]}")
             else:
-                print(f"\n\t Não há passageiros cadastrados para o voô {consulta_voo}.")
-            print(f"\n\t O voô {num_voo} possui {dados[4]} lugares disponíveis")
+                print(f"\n\t Não há passageiros cadastrados para o voo {consulta_voo}.")
+            print(f"\n\t O voo {num_voo} possui {dados[4]} lugares disponíveis")
 
 
 def venda_passagem():
@@ -163,24 +163,24 @@ def venda_passagem():
         num_voo2 = verificar_voo_compra()
 
         if (num_voo2 in passageiros[cpf_venda][1]):
-            print(f"\n => VOÔ JÁ FOI COMPRADO! INSIRA OUTRO CÓDIGO, POR FAVOR <=")
+            print(f"\n => VOO JÁ FOI COMPRADO! INSIRA OUTRO CÓDIGO, POR FAVOR <=")
         else:
             if num_voo2 in voos.keys():
                 if (voos[num_voo2][4] > 0):
                     voos[num_voo2][4] -= 1
                     voos[num_voo2][5].append(passageiros[cpf_venda][0]) 
                     passageiros[cpf_venda][1].append(num_voo2)
-                    print(f"\n => PASSAGEM PARA O VOÔ {num_voo2} VENDIDA COM SUCESSO! <= ")
+                    print(f"\n => PASSAGEM PARA O VOO {num_voo2} VENDIDA COM SUCESSO! <= ")
                 else:
-                    print("\n => NÃO HÁ LUGARES DISPONÍVEIS NESTE VOÔ <=")
+                    print("\n => NÃO HÁ LUGARES DISPONÍVEIS NESTE VOO <=")
             
     else:
-        qt_voos = int(input("\n Quantos voôs deseja comprar: "))
+        qt_voos = int(input("\n Quantos voos deseja comprar: "))
 
         passagens_compradas = [] 
         for n in range(qt_voos):
             mostrar_voos_disponiveis()
-            num_voo3 = int(input("\n\n Insira o número do voô: "))
+            num_voo3 = int(input("\n\n Insira o número do voo: "))
 
             if num_voo3 in voos.keys():
                 if voos[num_voo3][4] > 0:
@@ -192,9 +192,9 @@ def venda_passagem():
                             lista_voos[i][5] = depois
                     voos[num_voo3][5].append(nome)
                     passagens_compradas.append(num_voo3)
-                    print(f"\n => PASSAGEM PARA O VOÔ {num_voo3} VENDIDA COM SUCESSO PARA {nome}! <= ")
+                    print(f"\n => PASSAGEM PARA O VOO {num_voo3} VENDIDA COM SUCESSO PARA {nome}! <= ")
                 else:
-                    print(f"\n => NÃO HÁ LUGARES DISPONÍVEIS NO VOÔ {num_voo3} <=")
+                    print(f"\n => NÃO HÁ LUGARES DISPONÍVEIS NO VOO {num_voo3} <=")
         
         if (len(passagens_compradas) > 0):
             passageiros[cpf_venda] = [nome, fone, passagens_compradas]
@@ -220,11 +220,11 @@ def cancelar_passagem():
                     voos[voo_cancelar][4] += 1
                     if nome_passageiro_cancelar in voos[voo_cancelar][5]:
                         voos[voo_cancelar][5].remove(nome_passageiro_cancelar)
-                    print(f"\n => PASSAGEM DO VOÔ {voo_cancelar} PARA {nome_passageiro_cancelar} CANCELADA COM SUCESSO! <= ")
+                    print(f"\n => PASSAGEM DO VOO {voo_cancelar} PARA {nome_passageiro_cancelar} CANCELADA COM SUCESSO! <= ")
                 
                 if not passageiros[cpf_cancelamento][2]: 
                     del passageiros[cpf_cancelamento]
-                    print(f"\n => O PASSAGEIRO COM CPF {cpf_cancelamento} NÃO POSSUI PASSAGEM PARA O VOÔ {voo_cancelar} <= ")
+                    print(f"\n => O PASSAGEIRO COM CPF {cpf_cancelamento} NÃO POSSUI PASSAGEM PARA O VOO {voo_cancelar} <= ")
             else :
                 print("\n => PASSAGEM NÃO CANCELADA! VOCÊ SERÁ DIRECIONADO PARA O MENU PRINCIPAL <=")
     else:
@@ -250,13 +250,13 @@ def verificar_cpf():
 
 
 def verificar_voo():
-    num_voo = int(input("\n\n Insira o número do voô: "))
+    num_voo = int(input("\n\n Insira o número do voo: "))
     while (num_voo in voos.keys()):
-        print("\n => VOÔ JÁ EXISTENTE, TENTE NOVAMENTE <= ")
-        num_voo = int(input("\n\n Insira o número do voô: "))
+        print("\n => VOO JÁ EXISTENTE, TENTE NOVAMENTE <= ")
+        num_voo = int(input("\n\n Insira o número do voo: "))
     while (num_voo < 100 or num_voo > 99999):
-        print("\n => NÚMERO DE VOÔ INVÁLIDO! INSIRA UM NÚMERO DE 100 A 99999 <= ")
-        num_voo = int(input("\n\n Insira o número do voô: "))
+        print("\n => NÚMERO DE VOO INVÁLIDO! INSIRA UM NÚMERO DE 100 A 99999 <= ")
+        num_voo = int(input("\n\n Insira o número do voo: "))
 
     return num_voo
 
@@ -288,10 +288,10 @@ def verificar_telefone():
 
 
 def verificar_voo_compra():
-    num_voo = int(input("\n\n Insira o número do voô: "))
+    num_voo = int(input("\n\n Insira o número do voo: "))
     while (num_voo not in voos.keys()):
-        print("\n => VOÔ NÃO EXISTENTE, TENTE NOVAMENTE <= ")
-        num_voo = int(input("\n\n Insira o número do voô: "))
+        print("\n => VOO NÃO EXISTENTE, TENTE NOVAMENTE <= ")
+        num_voo = int(input("\n\n Insira o número do voo: "))
     
     return num_voo
 
